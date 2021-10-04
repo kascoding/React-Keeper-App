0c37fd630f89784bd65455a032a9b95a909823ff
Mc Donald kasonga <mulumbamack@gmail.com>
2021-10-04 14:49:10 -0700

    first commit

---
+++ build/precache-manifest.99efa54495766c94860e31569dd6622f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d7371247339a14fbdd61149939ec16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "f716634cca81eb78aac3",
+    "url": "/static/js/main.cee7a60b.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
